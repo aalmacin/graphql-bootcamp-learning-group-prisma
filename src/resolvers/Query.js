09bdac8628c6e647496306708e9b00bfa61364ac
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-03-21 20:45:37 -0400

    Add operation arguments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,16 @@
 export default {
         users(parent, args, {db: {users}, prisma}, info) {
+            const opArgs = {}
+            if(args.query) {
+                opArgs.where = {
+                    OR: [
+                        {name_contains: args.query},
+                        {email_contains: args.query}
+                    ]
+                }
+                return prisma.query.users(opArgs, info)
+            }
+
             return prisma.query.users(null, info)
 
             // // nothing, string, object
