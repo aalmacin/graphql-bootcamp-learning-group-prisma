ef8c902935353d96981a49fa35159df9aeb816a7
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-03-30 22:59:40 -0400

    Make prisma subscription work

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,28 +1,12 @@
 export default {
-  // count: {
-  //   subscribe(parent, args, { pubsub }, info) {
-  //     let count = 0;
-
-  //     setInterval(() => {
-  //       count++;
-  //       pubsub.publish("count", { count });
-  //     }, 1000);
-
-  //     return pubsub.asyncIterator("count");
-  //   },
-  // },
   post: {
-    subscribe(parent, {user: id}, {db: {users}, pubsub}, info) {
-      const user = users.find((u) => u.id === id);
-      if (!user) {
-        throw new Error("User not found");
-      }
-      return pubsub.asyncIterator(`post: ${id}`)
+    subscribe(parent, {user: id}, {db: {users}, pubsub, prisma}, info) {
+      return prisma.subscription.post({where: {node: {user: {id: id}}}}, info)
     }
   },
   comment: {
     subscribe(parent, { post: id }, { pubsub, db: { posts }, prisma }, info) {
-      return prisma.subscription.comment(null, info)
+      return prisma.subscription.comment({where: {node: {post: {id: id}}}}, info)
     },
   },
 };
