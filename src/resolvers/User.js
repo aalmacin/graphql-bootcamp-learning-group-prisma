da4d80d3aca7a61463daeab47cf6afa7072a21d1
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-04-11 00:22:43 -0400

    Add field lockdown

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,1 +1,13 @@
-export default {}
+import {getUserId} from '../utils/getUserId'
+
+export default {
+    email(parent, args, {request}, info) {
+        const userId = getUserId(request, false)
+        console.log("AQUI")
+
+        if(userId && userId === parent.id) {
+            return parent.email
+        }
+        return null
+    }
+}
