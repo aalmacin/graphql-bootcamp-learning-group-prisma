2711cd293a8fc92a2eb3ae3315786e4d18b4eb36
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-04-11 22:31:44 -0400

    Using fragments

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import Subscription from "./resolvers/Subscription";
-import Post from "./resolvers/Post";
-import User from "./resolvers/User";
-import Comment from "./resolvers/Comment";
+import {resolvers, fragmentReplacements} from "./resolvers";
 import prisma from "./prisma";
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    Post,
-    User,
-    Comment,
-  },
+  resolvers,
   context: (request) =>({
     db,
     pubsub,
     prisma,
     request
   }),
+  fragmentReplacements
 });
 
 server.start(() => {
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,13 +1,29 @@
 import {getUserId} from '../utils/getUserId'
 
 export default {
-    email(parent, args, {request}, info) {
-        const userId = getUserId(request, false)
-        console.log("AQUI")
-
-        if(userId && userId === parent.id) {
-            return parent.email
+    posts: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, {prisma}, info) {
+            return prisma.query.posts({
+                where: {
+                    published: true,
+                    author: {
+                        id: parent.id
+                    }
+                }
+            })
         }
-        return null
+    },
+    email: {
+        fragment: 'fragment userId on User { id }',
+            resolve(parent, args, {request}, info) {
+            const userId = getUserId(request, false)
+            console.log(parent)
+
+            if(userId && userId === parent.id) {
+                return parent.email
+            }
+            return null
+        }
     }
 }
